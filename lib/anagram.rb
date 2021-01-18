09ce2c452ced78226e65359e4374f98b0e327a4b
Ben Altman <altmanguitar@gmail.com>
2021-01-18 21:47:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,12 +8,16 @@ class Anagram
   end
 
   def match (array)
+  #return answers in an array
     matches = []
+  #compare each member of array with the word
     array.each do |item|
+  #.chars creates an array of the characters in a string and .sort alphabetizes them
       if item.chars.sort == @word.chars.sort
         matches << item
       end
     end
+  #remember to return the array!
     matches
   end
 
